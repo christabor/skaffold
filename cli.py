d4717cd0823852a8bd9bb3cf5e0c931f5eebf132
Chris Tabor <ctabor@zulily.com>
2015-05-16 11:14:24 -0700

    Use new style print.

--- cli.py
+++ cli.py
@@ -17,28 +17,28 @@ def from_scratch_django(fixture_data):
 
     # TODO: this is kind of weird
 
-    print 'Deleting existing django project (if applicable)...'
+    print('Deleting existing django project (if applicable)...')
     os.system('rm -r ' + project_name)
 
-    print 'Adding new django project...'
+    print('Adding new django project...')
     os.system('django-admin.py startproject ' + project_name)
 
-    print 'Generating new django app...'
+    print('Generating new django app...')
     new_app_django(fixture_data)
 
-    print 'Generating DB tables...'
+    print('Generating DB tables...')
     os.system('cd ' + project_name + ' && python manage.py syncdb --noinput')
 
-    print 'Generating DB test data...'
+    print('Generating DB test data...')
     os.system('python manage.py generate_fixtures')
 
-    print 'Jumping back down...'
+    print('Jumping back down...')
     os.system('cd ..')
 
     # TODO: Automatically fix the below issues for user.
     # TODO: test with making a bunch of apps, in config
 
-    print """
+    print("""
     Okay! Everything is done.
     You just need to update a few things...
 
@@ -53,14 +53,14 @@ def from_scratch_django(fixture_data):
 
     See the README for more details.
 
-    """.format(project_name, app_name)
+    """.format(project_name, app_name))
 
 
 def new_app_django(fixture_data):
     """A quick util to access the generator from command line"""
-    print 'Generating... app "{}" in project "{}"'.format(
+    print('Generating... app "{}" in project "{}"'.format(
         fixture_data['config']['app_name'],
-        fixture_data['config']['project_root'])
+        fixture_data['config']['project_root']))
 
     # TODO: add types for classes (with --type option, or config property),
     # instead of defaulting to django
@@ -71,7 +71,7 @@ def new_app_django(fixture_data):
 # Run when file is run
 try:
     if not sys.argv[2]:
-        print 'No JSON arguments supplied.'
+        print('No JSON arguments supplied.')
     if sys.argv[1] == '--json' and sys.argv[2].endswith('json'):
         json_file = sys.argv[2]
         with open(json_file, 'r') as json_data:
@@ -81,8 +81,8 @@ try:
             else:
                 new_app_django(fixture_data)
     else:
-        print '`{}` is not a valid .json file'.format(sys.argv[2])
+        print('`{}` is not a valid .json file'.format(sys.argv[2]))
 except IndexError:
-    print ('No arguments were specified.'
+    print(('No arguments were specified.'
            ' Please provided a JSON file with'
-           ' the `--json filename` parameter.')
+           ' the `--json filename` parameter.'))
